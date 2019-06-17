251fb6b8ed14d94007f4c0922f16122b37dc5fe3
Cryptious <martinowagey@gmail.com>
2019-06-17 13:45:51 +0700

    add Tugas 1 React

---
+++ src/Page/MenuProduk.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MenuProduk extends Component {
+  render (){
+    return(
+      <p>Ini Halaman Menu Produk</p>
+    );
+  }
+}
+export default MenuProduk;
---
+++ src/Page/MenuTentangKami.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MenuTentangKami extends Component {
+  render (){
+    return(
+      <p>Ini Halaman Menu Tentang Kami</p>
+    );
+  }
+}
+export default MenuTentangKami;
